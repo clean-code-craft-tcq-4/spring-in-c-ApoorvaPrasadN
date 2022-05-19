5063805a5964cf6abd7ec080ebe25d01f971c4bd
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-05-19 14:02:03 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -29,10 +29,10 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("raises alerts when max is greater than threshold") {
     // create additional .c and .h files
     // containing the emailAlerter, ledAlerter functions
-    int *emailAlerter, *ledAlerter;
-    emailAlerter = (void *)emailAlerter();
-    ledAlerter = (void *)ledAlerter();
-    alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
+    //int *emailAlerter, *ledAlerter;
+    //emailAlerter = (void *)emailAlerter();
+    //ledAlerter = (void *)ledAlerter();
+    //alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
