51cad51e3a22668c7c00463d8cbd8594b230f3e8
ApoorvaPrasadN <105204469+ApoorvaPrasadN@users.noreply.github.com>
2022-05-19 12:58:56 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,9 +7,9 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
-struct Statistics
+struct Stats
 {
   float average;
   float min;
   float max;
-}Stats;
+}computedStats;
